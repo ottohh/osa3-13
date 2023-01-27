1cdc6e31f95bd30482be30629b30403e6c7c0936
Otto Haarahiltunen <otto.haarahiltunen@tuni.fi>
2023-01-27 20:29:26 +0200

    server

---
+++ build/precache-manifest.67c89377f48a2a693773b51d1125a4f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d51f544c5ffedd4b9b32",
+    "url": "/static/css/main.9b694af0.chunk.css"
+  },
+  {
+    "revision": "d51f544c5ffedd4b9b32",
+    "url": "/static/js/main.d51f544c.chunk.js"
+  },
+  {
+    "revision": "5205776392b6e10b014b",
+    "url": "/static/js/1.52057763.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "713e69e68ea6dd736e679acbb8d9ec50",
+    "url": "/index.html"
+  }
+];
